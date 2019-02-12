878e8f6e8d063c2fc521f4664aa6621e6a54d814
devgkz <devg@ya.ru>
2019-02-12 18:00:34 +0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,23 @@
 'use strict'
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var del = require('del');
-var runSequence = require('run-sequence');
-var csso = require('gulp-csso');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+const runSequence = require('run-sequence');
+const csso = require('gulp-csso');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const webpack = require('webpack-stream');
 
 gulp.paths = {
+    src: 'src',
     dist: 'dist',
 };
 
 var paths = gulp.paths;
 
 gulp.task('sass', function () {
-    return gulp.src('./src/main.scss')
+    return gulp.src(paths.src+'/main.scss')
         .pipe(sass())
         .pipe(csso({
             restructure: true,
@@ -33,11 +35,11 @@ gulp.task('copy:webfonts', ['copy:webfonts-fa4', 'copy:webfonts-fa4css']);
 //gulp.task('copy:webfonts', ['copy:webfonts-fa5', 'copy:webfonts-fa5css']);
 
 gulp.task('copy:webfonts-fa4', function() {
-   return gulp.src('./src/font-awesome-4.7.0/fonts/**/*')
+   return gulp.src(paths.src+'/font-awesome-4.7.0/fonts/**/*')
    .pipe(gulp.dest(paths.dist+'/fonts'));
 });
 gulp.task('copy:webfonts-fa4css', function() {
-   return gulp.src('./src/font-awesome-4.7.0/css/font-awesome.min.css')
+   return gulp.src(paths.src+'/font-awesome-4.7.0/css/font-awesome.min.css')
    .pipe(gulp.dest(paths.dist+'/css'));
 });
 
@@ -52,21 +54,20 @@ gulp.task('copy:webfonts-fa5css', function() {
 
 
 gulp.task('copy:css', function() {
-   return gulp.src('./src/css/**/*')
+   return gulp.src(paths.src+'/css/**/*')
    .pipe(gulp.dest(paths.dist+'/css'));
 });
 
 gulp.task('scripts', function() {
   return gulp.src([
         //'./node_modules/jquery/dist/jquery.min.js', 
-        './src/js/common.js'
+        paths.src+'/js/common.js'
     ])
     .pipe(concat('common.js'))
     .pipe(uglify())
     .pipe(gulp.dest(paths.dist+'/js'));
 });
 
-const webpack = require('webpack-stream');
 
 gulp.task('copy:js', function() {
   return gulp.src([
@@ -90,8 +91,8 @@ gulp.task('build', function(callback) {
 });
 
 gulp.task('watch', function(callback) {
-    gulp.watch('./src/**/*.*', ['build']);
-    // gulp.watch('./src/**/*.scss', function(callback) {
+    gulp.watch(paths.src+'/**/*.*', ['build']);
+    // gulp.watch(paths.src+'/**/*.scss', function(callback) {
         // runSequence('sass', 'postcss', callback);
     // });
 });
